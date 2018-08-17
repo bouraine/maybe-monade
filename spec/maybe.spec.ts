fb409d4cda70fa4e611c311fb80d8c12b082fe15
bouraine <m.bouraine@live.fr>
2018-08-18 01:53:49 +0200

    add map function and tests gitignore

--- spec/maybe.spec.ts
+++ spec/maybe.spec.ts
@@ -1,42 +1,45 @@
-import Maybe from "./../src/Maybe";
+import Maybe, {Errors} from "../src/Maybe";
 
 describe("Maybe monad", () => {
-  it("should be empty", () => {
-    const intNum = Maybe.fromValue<number>(null);
-    const str = Maybe.fromValue<string>(null);
-    const strNotEmpty = Maybe.fromValue<string>("hello");
-    const noneVal = Maybe.none();
-    // Assert
-    expect(intNum.isEmpty()).toBeTruthy();
-    expect(intNum.getOrElse(0)).toEqual(0);
-    expect(str.isEmpty()).toBeTruthy();
-    expect(strNotEmpty.isEmpty()).toBeFalsy();
-    expect(noneVal.isEmpty()).toBeTruthy();
-  });
+    it("should be empty", () => {
+        const intNum = Maybe.fromValue<number>(null);
+        const str = Maybe.fromValue<string>(null);
+        const strNotEmpty = Maybe.fromValue<string>("hello");
+        const noneVal = Maybe.none();
+        // Assert
+        expect(intNum.isEmpty()).toBeTruthy();
+        expect(intNum.exists()).toBeFalsy();
+        expect(intNum.getOrElse(0)).toEqual(0);
+        expect(str.isEmpty()).toBeTruthy();
+        expect(strNotEmpty.isEmpty()).toBeFalsy();
+        expect(noneVal.isEmpty()).toBeTruthy();
+    });
 
-  it("should return the right value", () => {
-    const intNum = Maybe.fromValue<number>(12);
-    const val = intNum.getOrElse(null);
-    const str = Maybe.fromValue<string>("12");
-    const strVal = str.getOrElse(null);
-    // Assert
-    expect(val).toEqual(12);
-    expect(strVal).toEqual("12");
-  });
+    it("should return the right value", () => {
+        const intNum = Maybe.fromValue<number>(12);
+        const val = intNum.getOrElse(null);
+        const str = Maybe.fromValue<string>("12");
+        const strVal = str.getOrElse(null);
+        // Assert
+        expect(val).toEqual(12);
+        expect(strVal).toEqual("12");
+    });
 
-  it("should throw error", () => {
-    const errorMSg = "Provided value must not be empty";
-    try {
-      // Act
-      const nullVal = Maybe.some(null);
-      const emptyVal = Maybe.some(null);
-      const NanVal = Maybe.some(null);
-      // Assert
-      expect(nullVal).toThrowError(errorMSg);
-      expect(emptyVal).toThrowError(errorMSg);
-      expect(NanVal).toThrowError(errorMSg);
-    } catch (ex) {
-      expect(ex.message).toEqual(errorMSg);
-    }
-  });
+    it("should throw error", () => {
+        expect(() => Maybe.some(null)).toThrow(Errors.emptyValue);
+        expect(() => Maybe.some([])).toThrow(Errors.emptyValue);
+    });
+
+    it("should throw an error", () => {
+        expect(() => Maybe.some(null)).toThrow(Errors.emptyValue);
+    });
+
+    it("should return half", () => {
+        const just2 = Maybe.fromValue(3.0);
+        const add = (x: number) => x + 1;
+        const just3 = just2.map(add);
+        const div4 = (x: number) => x / 2;
+        expect(just3.get()).toEqual(3.0 + 1);
+        expect(just2.map(div4).get()).toEqual(1.5);
+    });
 });
--- src/Maybe.ts
+++ src/Maybe.ts
@@ -1,25 +1,44 @@
+type FMap<T> = (item: T) => T;
+export const Errors = {
+    emptyValue: "Provided value must not be empty or an array"
+};
+
 export default class Maybe<T> {
-  public static none<T>() {
-    return new Maybe<T>(null);
-  }
-  public static fromValue<T>(value: T) {
-    return value ? Maybe.some(value) : Maybe.none<T>();
-  }
-
-  public static some<T>(value: T) {
-    if (!value) {
-      throw Error("Provided value must not be empty");
+    public static none<T>() {
+        return new Maybe<T>(null);
     }
-    return new Maybe(value);
-  }
 
-  private constructor(private value: T | null) {}
+    public static fromValue<T>(value: T) {
+        return value ? Maybe.some(value) : Maybe.none<T>();
+    }
 
-  public getOrElse(defaultValue: T) {
-    return this.value === null ? defaultValue : this.value;
-  }
+    public static some<T>(value: T) {
+        if (!value || Array.isArray(value)) {
+            throw Error(Errors.emptyValue);
+        }
+        return new Maybe(value);
+    }
 
-  public isEmpty() {
-    return this.value === null;
-  }
+    private constructor(private value: T | null) {
+    }
+
+    public getOrElse(defaultValue: T) {
+        return this.value === null ? defaultValue : this.value;
+    }
+
+    public isEmpty() {
+        return this.value === null;
+    }
+
+    public exists() {
+        return this.value !== null;
+    }
+
+    public get() {
+        return this.value;
+    }
+
+    public map(fmap: FMap<T>): Maybe<T> {
+        return Maybe.fromValue(fmap(this.value));
+    }
 }
