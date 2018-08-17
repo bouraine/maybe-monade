512099e2da576138191fd34b33f1f4f02cad5bcf
bouraine <m.bouraine@live.fr>
2018-08-18 01:59:01 +0200

    gitignore

--- dist/Maybe.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Maybe<T> {
-  private value;
-  static none<T>(): Maybe<T>;
-  static fromValue<T>(value: T): Maybe<T>;
-  static some<T>(value: T): Maybe<T>;
-  private constructor();
-  getOrElse(defaultValue: T): T;
-  isEmpty(): boolean;
-}
