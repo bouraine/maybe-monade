f7bdea4622fc4c3e00787574c81173e318294de2
Bouraine <m.bouraine@live.fr>
2018-09-01 10:10:09 +0200

    index in root dir

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var maybe_1 = require("./dist/maybe");
+exports.Maybe = maybe_1.Maybe;
--- src/maybe.ts
+++ src/maybe.ts
@@ -5,115 +5,115 @@ import {ErrorMessages, isNullOrUndefined} from "./utils";
  */
 export class Maybe<T> {
 
-    /**
-     * Return an instance of Maybe wrapping an empty value
-     */
-    public static none<T>() {
-        return new Maybe<T>(null);
-    }
+  /**
+   * Return an instance of Maybe wrapping an empty value
+   */
+  public static none<T>() {
+    return new Maybe<T>(null);
+  }
 
-    /**
-     * return an instance of Maybe wrapping the provided value, otherwise return an instance of empty Maybe
-     * @param value value to wrap into a Maybe
-     */
-    public static fromValue<T>(value: T): Maybe<T> {
-        return isNullOrUndefined(value) ? Maybe.none<T>() : Maybe.some(value);
-    }
+  /**
+   * return an instance of Maybe wrapping the provided value, otherwise return an instance of empty Maybe
+   * @param value value to wrap into a Maybe
+   */
+  public static fromValue<T>(value: T): Maybe<T> {
+    return isNullOrUndefined(value) ? Maybe.none<T>() : Maybe.some(value);
+  }
 
-    /**
-     * if the provided value is not falsy, return an instance of Maybe wrapping a nonempty value,
-     * otherwise throw an error
-     * @param value the value to wrap in an instance of Maybe
-     */
-    public static some<T>(value: T): Maybe<T> {
-        if (isNullOrUndefined(value)) {
-            throw Error(ErrorMessages.emptyValue);
-        }
-        return new Maybe(value);
+  /**
+   * if the provided value is not falsy, return an instance of Maybe wrapping a nonempty value,
+   * otherwise throw an error
+   * @param value the value to wrap in an instance of Maybe
+   */
+  public static some<T>(value: T): Maybe<T> {
+    if (isNullOrUndefined(value)) {
+      throw Error(ErrorMessages.emptyValue);
     }
+    return new Maybe(value);
+  }
 
-    private readonly value: T;
+  private readonly value: T;
 
-    private constructor(value: T | null) {
-        this.value = value;
+  private constructor(value: T | null) {
+    this.value = value;
+  }
+
+  /**
+   * return true if the wrapped value is empty, false otherwise
+   */
+  public isEmpty() {
+    return isNullOrUndefined(this.value);
+  }
+
+  /**
+   * return true if the wrapped value is nonempty, false otherwise
+   */
+  public exists() {
+    return !isNullOrUndefined(this.value);
+  }
+
+  /**
+   * get the wrapped value
+   */
+  public get(): T {
+    if (this.exists()) {
+      return this.value;
+    } else {
+      throw new Error(ErrorMessages.getEmptyValue);
     }
+  }
 
-    /**
-     * return true if the wrapped value is empty, false otherwise
-     */
-    public isEmpty() {
-        return isNullOrUndefined(this.value);
-    }
+  /**
+   * return the wrapped value if nonempty, otherwise the provided default value.
+   */
+  public getOrElse(defaultValue: T): T {
+    return this.isEmpty() ? defaultValue : this.value;
+  }
 
-    /**
-     * return true if the wrapped value is nonempty, false otherwise
-     */
-    public exists() {
-        return !isNullOrUndefined(this.value);
-    }
+  /**
+   * return the value if nonempty, otherwise invoke alternative
+   * and return the result of that invocation.
+   * @param alternative the function to invoke
+   */
+  public orElse(alternative: () => Maybe<T>): Maybe<T> {
+    return this.exists() ? Maybe.some(this.value) : alternative();
+  }
 
-    /**
-     * get the wrapped value
-     */
-    public get(): T {
-        if (this.exists()) {
-            return this.value;
-        } else {
-            throw new Error(ErrorMessages.getEmptyValue);
-        }
-    }
+  /**
+   * if ths value exists, apply the provided mapping function to it,
+   * return an instance of Maybe wrapping the result.
+   * @param fmap the function to apply
+   */
+  public map<R>(fmap: (value: T) => R): Maybe<R> {
+    return this.exists() ? Maybe.some(fmap(this.value)) : Maybe.none();
+  }
 
-    /**
-     * return the wrapped value if nonempty, otherwise the provided default value.
-     */
-    public getOrElse(defaultValue: T): T {
-        return this.isEmpty() ? defaultValue : this.value;
-    }
+  /**
+   * if the wrapped value is nonempty, apply the provided mapping function to it,
+   * return that result, otherwise return an instance of empty Maybe.
+   * @param f the function to apply
+   */
+  public flatMap<R>(f: (value: T) => Maybe<R>): Maybe<R> {
+    return this.exists() ? f(this.value) : Maybe.none();
+  }
 
-    /**
-     * return the value if nonempty, otherwise invoke alternative
-     * and return the result of that invocation.
-     * @param alternative the function to invoke
-     */
-    public orElse(alternative: () => Maybe<T>): Maybe<T> {
-        return this.exists() ? Maybe.some(this.value) : alternative();
-    }
+  /**
+   * apply f to the wrapped value then return an instance of Maybe wrapping
+   * the value before applying the f function.
+   * f could be console.log for example.
+   * @param f function to apply
+   */
+  public do(f: (value: T) => void): Maybe<T> {
+    f(this.value);
+    return Maybe.fromValue(this.value);
+  }
 
-    /**
-     * if ths value exists, apply the provided mapping function to it,
-     * return an instance of Maybe wrapping the result.
-     * @param fmap the function to apply
-     */
-    public map<R>(fmap: (value: T) => R): Maybe<R> {
-        return this.exists() ? Maybe.some(fmap(this.value)) : Maybe.none();
-    }
-
-    /**
-     * if the wrapped value is nonempty, apply the provided mapping function to it,
-     * return that result, otherwise return an instance of empty Maybe.
-     * @param f the function to apply
-     */
-    public flatMap<R>(f: (value: T) => Maybe<R>): Maybe<R> {
-        return this.exists() ? f(this.value) : Maybe.none();
-    }
-
-    /**
-     * apply f to the wrapped value then return an instance of Maybe wrapping
-     * the value before applying the f function.
-     * f could be console.log for example.
-     * @param f function to apply
-     */
-    public do(f: (value: T) => void): Maybe<T> {
-        f(this.value);
-        return Maybe.fromValue(this.value);
-    }
-
-    /**
-     * if the wrapped value is nonempty, and the value matches the given predicate,
-     * return a Maybe wrapping the value, otherwise return an instance of empty Maybe
-     * @param predicate a predicate to apply to the value if nonempty
-     */
-    public filter(predicate: (x: T) => boolean): Maybe<T> {
-        return this.exists() && predicate(this.value) ? Maybe.some(this.value) : Maybe.none();
-    }
+  /**
+   * if the wrapped value is nonempty, and the value matches the given predicate,
+   * return a Maybe wrapping the value, otherwise return an instance of empty Maybe
+   * @param predicate a predicate to apply to the value if nonempty
+   */
+  public filter(predicate: (x: T) => boolean): Maybe<T> {
+    return this.exists() && predicate(this.value) ? Maybe.some(this.value) : Maybe.none();
+  }
 }
